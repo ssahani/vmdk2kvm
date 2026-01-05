cf229278c2a7c286ec1be2d42cd806d7eb7a9fc6
Susant Sahani <ssahani@redhat.com>
2026-01-05 18:17:55 +0530

    Add version
    Signed-off-by: Susant Sahani <ssahani@redhat.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vmdk2kvm",
-    version="3.1.0",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[l.strip() for l in open("requirements.txt", encoding="utf-8") if l.strip() and not l.startswith("#")],
     entry_points={"console_scripts": ["vmdk2kvm=vmdk2kvm.__main__:main"]},
